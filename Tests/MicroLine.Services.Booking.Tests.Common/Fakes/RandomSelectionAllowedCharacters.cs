dffa4e6f6e0bc064303bf05f9ac74f23be61f4b0
Faraz Azadi <farazazadi70@gmail.com>
2023-01-19 22:50:21 +0300

    Add Airport Fakers

---
+++ Tests/MicroLine.Services.Booking.Tests.Common/Fakes/RandomSelectionAllowedCharacters.cs
@@ -0,0 +1,8 @@
+﻿namespace MicroLine.Services.Booking.Tests.Common.Fakes;
+internal static class RandomSelectionAllowedCharacters
+{
+    internal const string All = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    internal const string DigitsAndLowerCaseLetters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    internal const string DigitsAndUpperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    internal const string UpperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
