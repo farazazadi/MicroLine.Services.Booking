7aded08d45fa43799299aa7700acacf967270882
Faraz Azadi <farazazadi70@gmail.com>
2023-01-25 15:30:44 +0300

    Add Integration event and its extension

---
+++ Source/MicroLine.Services.Booking.WebApi/Infrastructure/Integration/IntegrationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace MicroLine.Services.Booking.WebApi.Infrastructure.Integration;
+
+internal static class IntegrationExtensions
+{
+    private static readonly Dictionary<string,Type> CachedIntegrationEventTypes;
+
+    static IntegrationExtensions()
+    {
+        CachedIntegrationEventTypes = Assembly
+            .GetExecutingAssembly()
+            .GetTypes()
+            .Where(type => type.IsSubclassOf(typeof(IntegrationEvent)) && !type.IsAbstract)
+            .ToDictionary(type => type.Name, type => type);
+    }
+    
+    public static Type? GetCorrespondingIntegrationEventType(this string typeName)
+    {
+        CachedIntegrationEventTypes.TryGetValue(typeName, out var type);
+
+        return type;
+    }
+}
