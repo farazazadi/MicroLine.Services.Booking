e1d6df8d89527fc0f9dd1fa760249d2a41b8a72d
Faraz Azadi <farazazadi70@gmail.com>
2023-07-20 21:29:17 +0300

    Add get all Passengers endpoint

---
+++ Source/MicroLine.Services.Booking.WebApi/Features/Passengers/GetAllPassengers.cs
@@ -0,0 +1,63 @@
+﻿using MapsterMapper;
+using MediatR;
+using MicroLine.Services.Booking.Domain.Passengers;
+using MicroLine.Services.Booking.WebApi.Features.Passengers.DataTransferObjects;
+using MicroLine.Services.Booking.WebApi.Infrastructure.MongoDb;
+using MongoDB.Driver;
+
+namespace MicroLine.Services.Booking.WebApi.Features.Passengers;
+
+internal sealed class GetAllPassengers
+{
+    public static WebApplication MapEndpoint(WebApplication app)
+    {
+        app.MapGet("api/passengers", async (ISender sender, CancellationToken token) =>
+        {
+            IReadOnlyList<PassengerGroup> passengerGroups = await sender.Send(new Request(), token);
+            return Results.Ok(passengerGroups);
+        });
+        return app;
+    }
+
+
+    public sealed class PassengerGroup
+    {
+        public required string RelatedUserExternalId { get; init; }
+        public required IReadOnlyList<PassengerDto> Passengers { get; init; }
+    }
+
+
+    public record Request : IRequest<IReadOnlyList<PassengerGroup>>;
+
+
+    public sealed class RequestHandler : IRequestHandler<Request, IReadOnlyList<PassengerGroup>>
+    {
+        private readonly MongoService _mongoService;
+        private readonly IMapper _mapper;
+
+        public RequestHandler(MongoService mongoService, IMapper mapper)
+        {
+            _mongoService = mongoService;
+            _mapper = mapper;
+        }
+
+        public Task<IReadOnlyList<PassengerGroup>> Handle(Request request, CancellationToken token)
+        {
+
+            IReadOnlyList<PassengerGroup> passengerGroups = _mongoService
+                .GetCollection<Passenger>()
+                .AsQueryable()
+                .GroupBy(p => p.RelatedUserExternalId)
+                .ToList()
+                .Select(g => new PassengerGroup
+                {
+                    RelatedUserExternalId = g.Key.ToString(),
+                    Passengers = _mapper.Map<IReadOnlyList<PassengerDto>>(g.ToList())
+                })
+                .ToList();
+
+
+            return Task.FromResult(passengerGroups);
+        }
+    }
+}
