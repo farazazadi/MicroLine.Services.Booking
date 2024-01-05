b9ee8dd5dc5aebc845feeb0be4770b02209373f8
Faraz Azadi <farazazadi70@gmail.com>
2024-01-05 21:19:51 +0330

    Add OperationException

---
+++ Source/MicroLine.Services.Booking.WebApi/Common/Exceptions/OperationException.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MicroLine.Services.Booking.WebApi.Common.Exceptions;
+
+internal class OperationException : ApplicationExceptionBase
+{
+    public override string Code => nameof(OperationException);
+
+    public required string OperationName { get; init; }
+
+
+    [SetsRequiredMembers]
+    public OperationException(string operationName, string message) : base(message)
+        => OperationName = operationName;
+}
